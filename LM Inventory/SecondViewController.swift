9b6fcded7a6da1a32d36fd339f73120bdeda16f2
Zachary Quemore <quemorez@gmail.com>
2018-02-10 21:12:53 -0800

    Initial Commit

---
+++ LM Inventory/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  LM Inventory
+//
+//  Created by Zachary Quemore on 2/10/18.
+//  Copyright © 2018 Zachary Quemore. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
